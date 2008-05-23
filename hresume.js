c7f76a71c030e4f8e8b68138336d417f2586532b
Justin Henry <jhenry@robinhood.local>
2008-05-23 00:22:47 -0400

    enhancements to open/close of about box, formatting and text edits to about box; added references line to footer;

--- hresume.js
+++ hresume.js
@@ -1,28 +1,37 @@
 
 $(document).ready(function() {
 
-
     // on clicking the about link,
     // insert a dom element after the action-strip, give it a class name of about-slidedown
     // put the contents of about-copy into about-slidedown
     // toggle about-slidedown's visibility, with a slide down effect
-    // TODO: make hide/show more modular
-    // TODO: add a close button
-    // TODO: fade bg color in and out
+    // TODO: clean this up
+    // TODO: put a fade on the bg color
     $('.documentation a.toggle-about-box').toggle(function() {
-    
-        $('.about-slidedown').remove();
-        $("<div>").addClass('about-slidedown').insertAfter('.action-strip').hide();
-    
-        $('.about-slidedown').html($('#about-copy').html()).slideDown();
-        $('body').css('background-color', '#cfcfcf');
-    
+          return show_about_box();
     }, function() {
-    
-        $('.about-slidedown').slideUp();
-        $('body').css('background-color', '#ffffff');
-    
+        return close_about_box();
     });
+    
 });
 
+function show_about_box () {
+    $('.about-slidedown').remove();
+    $("<div>").addClass('about-slidedown').insertAfter('.action-strip').hide();
 
+    $('.about-slidedown').html($('#about-copy').html()).slideDown();
+    $('body').css('background-color', '#cfcfcf');
+    
+    $('.close-button,#hd,#bd').click(function() {
+        $('.documentation a.toggle-about-box').trigger("click"); 
+        $('.close-button,#hd,#bd').unbind('click'); // no open/close after the window is closed.
+        return false;
+    });
+    return false;
+}
+
+function close_about_box () {
+    $('.about-slidedown').slideUp();
+    $('body').css('background-color', '#ffffff');
+    return false
+}
