eb245cff3ebdb4f57119d9e25a47a5f7ae45338c
Justin Henry <jhenry@robinhood.local>
2008-10-02 17:33:32 -0400

    tweaked slide down event fade effect

--- hresume.js
+++ hresume.js
@@ -9,10 +9,12 @@ $(document).ready(function() {
     });
 });
 
+// Fade body background into grey.
 // Insert a hidden dom element after the action-strip.
 // Put the contents of #about-copy into #about-slidedown.
 // Show #about-slidedown, with a slide down effect.
 function open_about_box () {
+    $('body').animate({ backgroundColor: "#cfcfcf" }, "fast");
     $('<div id="about-slidedown">').insertAfter('.action-strip').hide();
     $('#about-slidedown').html($('#about-copy').html()).slideDown({duration: "fast", complete: cleanup_slidedown});
     
@@ -23,10 +25,8 @@ function exit_about_box () {
     $('#about-slidedown').slideUp({duration: "fast", complete: cleanup_slideup});
 }
 
-// Fade body background into grey.
 // Bind click events for closing about box.
 function cleanup_slidedown () {
-    $('body').animate({ backgroundColor: "#cfcfcf" }, "fast");
     $('#return a, #hd, #bd').click(function() {
           $('.documentation a.toggle-about-box').trigger("click"); 
           return false;
